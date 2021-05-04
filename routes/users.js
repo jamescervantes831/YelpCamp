5862ab4395fa6b0a500d6317adc3c0bb65379e3a
James Cervantes <jccervantes831@gmail.com>
2021-05-04 18:25:42 -0400

    added authentication to app

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../model/user');
+const catchAsync = require('../utils/catchAsync')
 
+router.get('/login', (req, res) =>{
+    res.render('users/login');
+})
 router.get('/register', (req, res) =>{
     res.render('users/register');
 })
 
-router.post('/register', (req, res) =>{
-    res.send(req.body.user)
+router.post('/register', catchAsync(async (req, res, next) =>{
+    try{
+        const { email, username, password } = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err)  return next(err);
+            req.flash('success', 'Welcome to YelpCamp!')
+            res.redirect('/campground')
+        })
+    }catch(e){
+        req.flash('error', e.message);
+        res.redirect('register')
+    }
+}))
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', "Welcome back!")
+    const redirectUrl = req.session.returnTo || '/campground'
+    delete req.session.returnTo;
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campground')
 })
 
 module.exports = router
